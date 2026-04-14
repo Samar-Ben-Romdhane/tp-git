865cd476f337b46e69a85cf3bc672625052fb506
Samar <samar8brm@gmail.com>
2026-04-14 21:41:05 +0100

    Ajout des arguments en ligne de commande : message et nbAffichage

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,8 +1,32 @@
-//TODO indiquer ce qui fait le programme 
+#include "fonction-bienvenue.h"
 #include <iostream>
-int main()
+#include <cstdlib>
+#include <string>
+
+int main(int argc, char* argv[])
 {
-std::cout <<"Hello" << std::endl;
-//TODO Afficher un message bienvenue 
-return 0;
+    if (argc == 1)
+    {
+        afficherBienvenue();
+    }
+    else if (argc == 2)
+    {
+        std::string message = argv[1];
+        afficherBienvenue(message);
+    }
+    else if (argc == 3)
+    {
+        std::string message = argv[1];
+        int nbAffichage = std::atoi(argv[2]);
+        if (nbAffichage > 0)
+            afficherBienvenue(message, nbAffichage);
+        else
+            std::cerr << "Erreur : le nombre de répétitions doit être positif." << std::endl;
+    }
+    else
+    {
+        std::cerr << "Usage: " << argv[0] << " [message] [nbAffichage]" << std::endl;
+        return 1;
+    }
+    return 0;
 }
